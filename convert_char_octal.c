a31503a461ba00b3b53b0d15b0723eb02aced453
vuille_f <florian.vuillemot@epitech.eu>
2015-11-11 08:43:07 +0100

    S

--- convert_char_octal.c
+++ convert_char_octal.c
@@ -5,18 +5,19 @@
 ** Login   <vuille_f@epitech.net>
 ** 
 ** Started on  Tue Nov 10 20:39:21 2015 Florian Vuillemot
-** Last update Tue Nov 10 21:05:48 2015 Florian Vuillemot
+** Last update Wed Nov 11 08:42:53 2015 Florian Vuillemot
 */
 
 #include "my_string.h"
 #include "struct_string.h"
+#include <stdarg.h>
 
 static char		char_to_octal(char convert, int curs)
 {
-  unsigned int		end;
-  unsigned int		res;
-  unsigned int		one;
-  unsigned int		pow;
+  int			end;
+  int			res;
+  char			one;
+  int			pow;
 
   one = 1;
   end = curs + 3;
@@ -24,7 +25,7 @@ static char		char_to_octal(char convert, int curs)
   pow = 1;
   while (curs < end && curs < sizeof(char) * 8)
     {
-      if (one & (convert << curs))
+      if (convert & (one << curs))
 	res = pow + res;
       pow = pow * 2;
       curs = curs + 1;
@@ -33,40 +34,50 @@ static char		char_to_octal(char convert, int curs)
 }
 
 t_string		*convert_char_octal(t_string *string, char convert,
-					    unsigned int *cursor)
+					    unsigned int cursor)
 {
   int			curs;
 
   if (!string || !string->string)
     return (NULL);
-  curs = 0;
-  if (string->len < *cursor + 4)
+  if (string->len < cursor + 4)
     if ((string = realloc_string(string)) == NULL)
       return (NULL);
-  string =
-    add_elem_to_string(string, *cursor, '\\');
-  *cursor = *cursor + 1;
-  curs = sizeof(char) * 8 - 3;
-  while (curs > -1)
+  string = add_elem_to_string(string, cursor, '\\');
+  cursor = cursor + 1;
+  curs = 0;
+  while (curs < sizeof(char) * 8)
     {
       string =
-	add_elem_to_string(string, *cursor, char_to_octal(convert, curs));
-      curs = curs - 3;
-      *cursor = *cursor + 1;
+	add_elem_to_string(string, cursor, char_to_octal(convert, curs));
+      curs = curs + 3;
     }
+  cursor = cursor + 3;
   return (string);
 }
 
-
-int main()
+char			*flag_s_maj(va_list *list, int *type)
 {
-  t_string *string;
-  unsigned int i;
+  t_string		*string;
+  char			*res;
+  int			curs;
+  char			convert;
 
-  i = 0;
-  string = create_string("coucou");
-  printf("string : %s\n",string->string);
-  string = convert_char_octal(string, 34, &i);
-  printf("string : %s\n",string->string);
-  return (0);
+  if (!list)
+    return (NULL);
+  if ((res = va_arg(list, char *)) == NULL ||
+      (string = create_string(res)) == NULL)
+    return (my_strdup("(null)"));
+  curs = -1;
+  while (string->string[(curs = curs + 1)])
+    if (string->string[curs] < 32 || string->string[curs] >= 127)
+      {
+	convert = string->string[curs];
+	string = remove_elem_to_string(string, curs);
+	if (convert_char_octal(string, convert, curs) == NULL)
+	  return (NULL);
+      }
+  res = my_strdup(string->string);
+  free_string(string);
+  return (res);
 }
